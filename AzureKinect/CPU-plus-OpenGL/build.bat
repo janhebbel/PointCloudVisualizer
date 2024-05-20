4d8ac04393faac19c4db79997658732f22cba7de
Jan Hebbel Aguiar <jan.hebbel@gmail.com>
2024-05-20 09:48:13 +0200

    Sameified the build scripts.

--- AzureKinect/CPU-plus-OpenGL/build.bat
+++ AzureKinect/CPU-plus-OpenGL/build.bat
@@ -1,9 +1,5 @@
 @echo off
 
-if not defined DevEnvDir (
-    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 >NUL
-)
-
 IF NOT EXIST build mkdir build
 pushd build
 
@@ -11,12 +7,17 @@ set files=../code/main.c
 set compile_flags=/std:c11 /nologo /GR- /EHa- /Oi /WX /W4 /wd4100 /wd4189 /external:anglebrackets /external:W0 /FC /I..\third_party\windows
 set linker_flags=/opt:ref /subsystem:console user32.lib gdi32.lib shell32.lib opengl32.lib ..\lib\k4a.lib ..\lib\glfw3_mt.lib
 
+echo Building...
+echo:
+
 REM Debug Build:
-echo Debug Build
+echo Debug
 cl %compile_flags% /MT /Od /Z7 /DDEBUG /Fe"debug" %files% /link %linker_flags%
 
+echo:
+
 REM Release Build
-echo Release Build
+echo Release
 cl %compile_flags% /MT /O2 /DRELEASE /Fe"release" %files% /link %linker_flags%
 
 popd
--- AzureKinect/OpenCL/build.bat
+++ AzureKinect/OpenCL/build.bat
@@ -1,9 +1,5 @@
 @echo off
 
-if not defined DevEnvDir (
-    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 >NUL
-)
-
 IF NOT EXIST build mkdir build
 pushd build
 
